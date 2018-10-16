f1c37e0cef95f12af5d2ac06c5e3109c73a7d750
Thomas Shehan <thomas.shehan88@gmail.com>
2018-10-16 12:30:17 -0500

    add tests

--- src/components/tests/top-nav.test.js
+++ src/components/tests/top-nav.test.js
@@ -6,3 +6,18 @@ import TopNav from '../top-nav'
 it('Renders without crashing', () => {
   shallow(<TopNav />);
 });
+
+it('Should fire onRestartGame when "New Game" is clicked', () => {
+  const callback = jest.fn();
+  const wrapper = shallow(<TopNav onRestartGame={callback} />)
+  wrapper.find('.new').simulate('click');
+  expect(callback).toHaveBeenCalled();
+});
+
+it('Should fire onGenerateAuralUpdate when "Hear state of game" is clicked', () => {
+  const callback = jest.fn();
+  const wrapper = shallow(<TopNav onGenerateAuralUpdate={callback} />);
+  wrapper.find('.status-link').simulate('click');
+  expect(callback).toHaveBeenCalled();
+});
+
